ead8271342f3ec376b0fcd7efa624bf9e42fdb32
zhiyongfeng <88239962@qq.com>
2015-08-27 09:36:53 +0800

    add

---
+++ src/main/java/com.eleme.pro/MainClass_01.java
@@ -0,0 +1,10 @@
+package com.eleme.pro;
+
+/**
+ * Created by lz on 2015/8/27.
+ */
+public class MainClass_01 {
+    public static void main(String arg[]) {
+        System.out.println("TestMainClass............");
+    }
+}
